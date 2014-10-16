cfdca96132ab9b173267b3c863ecde0a8735615c
Boris Kaul <localvoid@gmail.com>
2014-10-16 10:51:44 +0700

    Add Location Database implementation

---
+++ pkgs/timezone/lib/src/location_database.dart
@@ -0,0 +1,78 @@
+// Copyright (c) 2014, the timezone project authors. Please see the AUTHORS
+// file for details. All rights reserved. Use of this source code is governed
+// by a BSD-style license that can be found in the LICENSE file.
+
+/// Locations database
+part of timezone;
+
+/// [LocationDatabase] provides interface to find [Location]s by their name.
+///
+/// ```dart
+/// List<int> data = load(); // load database
+///
+/// LocationDatabase db = new LocationDatabase.fromBytes(data);
+/// Location loc = db.get('US/Eastern');
+/// ```
+///
+class LocationDatabase {
+  /// Mapping between [Location] name and [Location].
+  final Map<String, Location> _locations = new HashMap<String, Location>();
+
+  LocationDatabase();
+
+  factory LocationDatabase.fromBytes(List<int> rawData) {
+    final data =
+        rawData is Uint8List ? rawData : new Uint8List.fromList(rawData);
+
+    final bdata =
+        data.buffer.asByteData(data.offsetInBytes, data.lengthInBytes);
+
+    final result = new LocationDatabase();
+
+    var offset = 0;
+    while (offset < data.length) {
+      final length = bdata.getUint32(offset);
+      final locationBytes = new Uint8List.view(bdata.buffer, offset, length);
+      final location = new Location.fromBytes(locationBytes);
+      offset += 4 + length;
+      result.add(location);
+    }
+
+    return result;
+  }
+
+  /// Add [Location] to the database.
+  void add(Location location) {
+    _locations[location.name] = location;
+  }
+
+  /// Find [Location] by its name.
+  Location get(String name) {
+    return _locations[name];
+  }
+
+  /// Serialize [LocationDatabase] in binary format.
+  List<int> toBytes() {
+    final locationsInBytes = [];
+    var bufferLength = 0;
+
+    for (final l in _locations.values) {
+      final b = l.toBytes();
+      locationsInBytes.add(b);
+      bufferLength += 4 + b.length;
+    }
+
+    final r = new Uint8List(bufferLength);
+    final rb = r.buffer.asByteData();
+
+    var offset = 0;
+    for (final b in locationsInBytes) {
+      rb.setUint32(offset, b.length);
+      r.setAll(offset + 4, b);
+
+      offset += 4 + b.length;
+    }
+
+    return r;
+  }
+}
--- pkgs/timezone/lib/timezone.dart
+++ pkgs/timezone/lib/timezone.dart
@@ -5,3 +5,4 @@ import 'dart:collection';
 import 'dart:typed_data';
 
 part 'package:timezone/src/location.dart';
+part 'package:timezone/src/location_database.dart';
