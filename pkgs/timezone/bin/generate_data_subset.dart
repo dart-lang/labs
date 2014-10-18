4c74eef98fef262c9be8745510bc069f50ab48d8
Boris Kaul <localvoid@gmail.com>
2014-10-18 21:16:38 +0700

    Print reports after generating data subsets

--- pkgs/timezone/bin/generate_data_subset.dart
+++ pkgs/timezone/bin/generate_data_subset.dart
@@ -4,8 +4,16 @@
 
 import 'dart:io';
 import 'package:args/args.dart';
+import 'package:tuple/tuple.dart';
 import 'package:timezone/standalone.dart';
 import 'package:timezone/timezone.dart';
+
+class Report {
+  int originalLocationsCount = 0;
+  int originalTransitionsCount = 0;
+  int newLocationsCount = 0;
+  int newTransitionsCount = 0;
+}
 
 /// Maximum value for time instants.
 const int _omega = 8640000000000000;
@@ -13,12 +21,17 @@ const int _omega = 8640000000000000;
 /// Minimum value for time instants.
 const int _alpha = -_omega;
 
-LocationDatabase filterTimeZoneData(LocationDatabase db, {int dateFrom: _alpha,
-    int dateTo: _omega}) {
+Tuple2<LocationDatabase, Report> filterTimeZoneData(LocationDatabase db,
+    {int dateFrom: _alpha, int dateTo: _omega}) {
+  final report = new Report();
   final result = new LocationDatabase();
+
+  report.originalLocationsCount = db.locations.length;
 
   for (final l in db.locations.values) {
     final transitionsCount = l.transitionAt.length;
+    report.originalTransitionsCount += transitionsCount;
+
     final newTransitionAt = [];
     final newTransitionZone = [];
 
@@ -38,11 +51,13 @@ LocationDatabase filterTimeZoneData(LocationDatabase db, {int dateFrom: _alpha,
       newTransitionAt.add(_alpha);
       newTransitionZone.add(l.transitionZone[i]);
       i++;
+      report.newTransitionsCount++;
 
       while (i < transitionsCount && l.transitionAt[i] <= dateTo) {
         newTransitionAt.add(l.transitionAt[i]);
         newTransitionZone.add(l.transitionZone[i]);
         i++;
+        report.newTransitionsCount++;
       }
     } else {
       newTransitionAt.add(_alpha);
@@ -51,9 +66,10 @@ LocationDatabase filterTimeZoneData(LocationDatabase db, {int dateFrom: _alpha,
 
     result.add(
         new Location(l.name, newTransitionAt, newTransitionZone, l.zones));
+    report.newLocationsCount++;
   }
 
-  return result;
+  return new Tuple2(result, report);
 }
 
 void main(List<String> arguments) {
@@ -98,12 +114,16 @@ void main(List<String> arguments) {
   }
 
   initializeTimeZone(source).then((_) {
-    final newLocationDatabase = filterTimeZoneData(LocationDatabase.instance,
+    final result = filterTimeZoneData(LocationDatabase.instance,
         dateFrom: from,
         dateTo: to);
 
-    return new File(output).writeAsBytes(newLocationDatabase.toBytes()).then((_) {
-      print('Finished');
+    final newDB = result.i1;
+    final report = result.i2;
+
+    return new File(output).writeAsBytes(newDB.toBytes()).then((_) {
+      print('Locations: ${report.originalLocationsCount} => ${report.newLocationsCount}');
+      print('Transitions: ${report.originalTransitionsCount} => ${report.newTransitionsCount}');
       exit(0);
     });
 
