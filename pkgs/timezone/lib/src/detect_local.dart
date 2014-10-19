44c53ad4720fa3e03b67464a35d606375642583a
Boris Kaul <localvoid@gmail.com>
2014-10-19 12:00:29 +0700

    Add comments

--- pkgs/timezone/lib/src/detect_local.dart
+++ pkgs/timezone/lib/src/detect_local.dart
@@ -1,3 +1,9 @@
+// Copyright (c) 2014, the timezone project authors. Please see the AUTHORS
+// file for details. All rights reserved. Use of this source code is governed
+// by a BSD-style license that can be found in the LICENSE file.
+
+/// Look at https://bitbucket.org/pellepim/jstimezonedetect for the possible
+/// better solution.
 part of timezone;
 
 /// Function to detect local [Location]
