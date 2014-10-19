a3d853d6c07a6d22c0dad53b05c6d8ea884d1629
Boris Kaul <localvoid@gmail.com>
2014-10-19 13:47:01 +0700

    Add script to dump location data

---
+++ pkgs/timezone/tool/zone_dump.dart
@@ -0,0 +1,36 @@
+/// Print Location details
+import 'dart:io';
+import 'package:args/args.dart';
+import 'package:timezone/standalone.dart';
+import 'package:timezone/timezone.dart';
+
+void main(List<String> arguments) {
+  // Parse CLI arguments
+  final parser = new ArgParser()
+  ..addOption('source', abbr: 'd', defaultsTo: 'packages/timezone/data/$dataDefaultFilename')
+  ..addOption('location', abbr: 'l');
+
+  final argResults = parser.parse(arguments);
+
+  final String source = argResults['source'];
+  final String location = argResults['location'];
+
+  if (source.isEmpty || location == null || location.isEmpty) {
+    print(parser.getUsage());
+    exit(64);
+  }
+
+  initializeTimeZone(source).then((_) {
+    final l = getLocation(location);
+
+    for (var i = 0; i < l.transitionAt.length; i++) {
+      final t = l.transitionAt[i];
+      final z = l.zones[l.transitionZone[i]];
+
+      final dt = new DateTime.fromMillisecondsSinceEpoch(t, isUtc: true);
+      print('${dt.toIso8601String()} $z');
+    }
+
+    exit(0);
+  });
+}
