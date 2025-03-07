2d89661c13cf3467aba6a6695c4f7e234fe6342e
Brian Quinlan <bquinlan@google.com>
2025-03-06 16:53:21 -0800

    Skeleton for `package:io_file`

---
+++ pkgs/io_file/test/io_file_test.dart
@@ -0,0 +1,16 @@
+import 'package:io_file/io_file.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
