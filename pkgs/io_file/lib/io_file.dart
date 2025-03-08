03cf9f07f74a046999882ccd5eab7811b6ddaddb
Brian Quinlan <bquinlan@google.com>
2025-03-07 18:41:28 -0800

    Update io_file.dart

--- pkgs/io_file/lib/io_file.dart
+++ pkgs/io_file/lib/io_file.dart
@@ -2,7 +2,5 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// TODO: Add a description for the package.
-
 export 'src/file_system.dart';
 export 'src/posix_file_system.dart';
