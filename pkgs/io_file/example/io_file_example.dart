bd696c62c04296ca9292e1aa5080120286430fba
Brian Quinlan <bquinlan@google.com>
2025-03-07 20:27:07 -0800

    Update io_file_example.dart

--- pkgs/io_file/example/io_file_example.dart
+++ pkgs/io_file/example/io_file_example.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:io_file/io_file.dart';
+import 'package:io_file/posix_file_system.dart';
 
 void main() {
   // TODO: Create a better example.
