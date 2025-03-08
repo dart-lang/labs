f42695e5296248b97c86996126e2214407ff47c9
Brian Quinlan <bquinlan@google.com>
2025-03-07 19:17:03 -0800

    Update rename_test.dart

--- pkgs/io_file/test/rename_test.dart
+++ pkgs/io_file/test/rename_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('posix')
+library;
+
 import 'dart:io';
 import 'package:io_file/io_file.dart';
 import 'package:test/test.dart';
