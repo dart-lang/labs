4817ded4a42e008908d549023663ca2b7d3dfaf0
Brian Quinlan <bquinlan@google.com>
2025-04-02 14:51:04 -0700

    Docs

--- pkgs/io_file/mobile_test/integration_test/combined_test.dart
+++ pkgs/io_file/mobile_test/integration_test/combined_test.dart
@@ -4,5 +4,6 @@ import '../../test/read_as_bytes_test.dart' as read_as_bytes;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  // TODO(brianquinlan): Enable more tests.
   read_as_bytes.main();
 }
