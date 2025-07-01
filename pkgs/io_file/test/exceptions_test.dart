525ce08dbbd6ee12f3d914b656b5d90e3190d694
Brian Quinlan <bquinlan@google.com>
2025-07-01 13:39:57 -0700

    Design a exception hierarchy for io_file (#250)

---
+++ pkgs/io_file/test/exceptions_test.dart
@@ -0,0 +1,71 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+@TestOn('vm')
+library;
+
+import 'package:io_file/src/exceptions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('exceptions', () {
+    test('basic', () {
+      expect(
+        const IOFileException('cannot open file').toString(),
+        'IOFileException: cannot open file',
+      );
+    });
+
+    test('with path1', () {
+      expect(
+        const IOFileException('cannot open file', path1: '/foo/bar').toString(),
+        'IOFileException: cannot open file, path1="/foo/bar"',
+      );
+    });
+
+    test('with path2', () {
+      expect(
+        const IOFileException('cannot open file', path2: '/foo/bar').toString(),
+        'IOFileException: cannot open file, path2="/foo/bar"',
+      );
+    });
+
+    test('with path1 and path2', () {
+      expect(
+        const IOFileException(
+          'cannot rename file',
+          path1: '/foo/baz',
+          path2: '/foo/bar',
+        ).toString(),
+        'IOFileException: cannot rename file, '
+        'path1="/foo/baz", path2="/foo/bar"',
+      );
+    });
+
+    test('system call', () {
+      expect(
+        const IOFileException(
+          'cannot open file',
+          systemCall: SystemCallError('open', 13, 'permission denied'),
+        ).toString(),
+        'IOFileException: cannot open file '
+        '(open: permission denied, errorCode=13)',
+      );
+    });
+
+    test('all arguments', () {
+      expect(
+        const IOFileException(
+          'cannot rename file',
+          path1: '/foo/baz',
+          path2: '/foo/bar',
+          systemCall: SystemCallError('rename', 13, 'permission denied'),
+        ).toString(),
+        'IOFileException: cannot rename file, '
+        'path1="/foo/baz", path2="/foo/bar" '
+        '(rename: permission denied, errorCode=13)',
+      );
+    });
+  });
+}
