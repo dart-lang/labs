383a038a29603d5f039e4e50e8e41eef4d4822fa
Brian Quinlan <bquinlan@google.com>
2025-07-11 09:17:53 -0700

    Support recursively deleting long file names on Windows (#256)

--- pkgs/io_file/test/remove_directory_tree_test.dart
+++ pkgs/io_file/test/remove_directory_tree_test.dart
@@ -44,49 +44,40 @@ void main() {
       );
     });
 
-    test(
-      'absolute path, long directory name',
-      () {
-        // On Windows:
-        // When using an API to create a directory, the specified path cannot be
-        // so long that you cannot append an 8.3 file name (that is, the
-        // directory name cannot exceed MAX_PATH minus 12).
-        final dirname =
-            'd' * (io.Platform.isWindows ? win32.MAX_PATH - 12 : 255);
-        final path = p.join(tmp, dirname);
-        io.Directory(path).createSync();
-        io.File('$path/file').writeAsStringSync('Hello World!');
+    test('absolute path, long directory name', () {
+      // On Windows:
+      // When using an API to create a directory, the specified path cannot be
+      // so long that you cannot append an 8.3 file name (that is, the
+      // directory name cannot exceed MAX_PATH minus 12).
+      final dirname = 'd' * (io.Platform.isWindows ? win32.MAX_PATH - 12 : 255);
+      final path = p.join(tmp, dirname);
+      io.Directory(path).createSync();
+      io.File('$path/file').writeAsStringSync('Hello World!');
 
-        fileSystem.removeDirectoryTree(path);
+      fileSystem.removeDirectoryTree(path);
 
-        expect(
-          io.FileSystemEntity.typeSync(path),
-          io.FileSystemEntityType.notFound,
-        );
-      },
-      skip: io.Platform.isWindows ? 'TODO(bquinlan): make this pass' : false,
-    );
+      expect(
+        io.FileSystemEntity.typeSync(path),
+        io.FileSystemEntityType.notFound,
+      );
+    });
 
-    test(
-      'relative path, long directory name',
-      () {
-        // On Windows:
-        // When using an API to create a directory, the specified path cannot be
-        // so long that you cannot append an 8.3 file name (that is, the
-        // directory name cannot exceed MAX_PATH minus 12).
-        final path = 'd' * (io.Platform.isWindows ? win32.MAX_PATH - 12 : 255);
-        io.Directory(path).createSync();
-        io.File('$path/file').writeAsStringSync('Hello World!');
+    test('relative path, long directory name', () {
+      // On Windows:
+      // When using an API to create a directory, the specified path cannot be
+      // so long that you cannot append an 8.3 file name (that is, the
+      // directory name cannot exceed MAX_PATH minus 12).
+      final path = 'd' * (io.Platform.isWindows ? win32.MAX_PATH - 12 : 255);
+      io.Directory(path).createSync();
+      io.File('$path/file').writeAsStringSync('Hello World!');
 
-        fileSystem.removeDirectoryTree(path);
+      fileSystem.removeDirectoryTree(path);
 
-        expect(
-          io.FileSystemEntity.typeSync(path),
-          io.FileSystemEntityType.notFound,
-        );
-      },
-      skip: io.Platform.isWindows ? 'TODO(bquinlan) make this pass' : false,
-    );
+      expect(
+        io.FileSystemEntity.typeSync(path),
+        io.FileSystemEntityType.notFound,
+      );
+    });
 
     test('contains single file', () {
       final path = '$tmp/dir';
@@ -147,6 +138,42 @@ void main() {
 
       final path = '$tmp/dir';
       createTree(path, 5);
+
+      fileSystem.removeDirectoryTree(path);
+
+      expect(
+        io.FileSystemEntity.typeSync(path),
+        io.FileSystemEntityType.notFound,
+      );
+    });
+
+    test('complex tree of long paths', () {
+      // On Windows:
+      // When using an API to create a directory, the specified path cannot be
+      // so long that you cannot append an 8.3 file name (that is, the
+      // directory name cannot exceed MAX_PATH minus 12).
+      void createTree(String path, int depth) {
+        io.Directory(path).createSync();
+
+        final filePath = 'f' * 255;
+        final fileLinkPath = 'l' * 255;
+        final directoryPath =
+            'd' * (io.Platform.isWindows ? win32.MAX_PATH - 12 : 255);
+        final directoryLinkPath =
+            's' * (io.Platform.isWindows ? win32.MAX_PATH - 12 : 255);
+
+        io.File('$path/$filePath').writeAsStringSync('Hello World');
+        io.Link('$path/$fileLinkPath').createSync('$path/file1');
+        io.Link('$path/$directoryLinkPath').createSync(path);
+
+        if (depth > 0) {
+          createTree('$path/$directoryPath', depth - 1);
+        }
+      }
+
+      final path = '$tmp/dir';
+      // macOS has a maximum path length of 1024 characters.
+      createTree(path, 2);
 
       fileSystem.removeDirectoryTree(path);
 
