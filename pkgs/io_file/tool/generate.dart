595b81eeb57c5977d8b0c2b95b63939ffae3b1c1
Brian Quinlan <bquinlan@google.com>
2025-07-22 10:58:10 -0700

    Add a generator script to regenerate native bindings (#264)

---
+++ pkgs/io_file/tool/generate.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Run all code generation and formatting steps.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ffigen/src/executables/ffigen.dart' as ffigen;
+import 'build_constants.dart' as build_constants;
+
+void _formatFile(String path) {
+  final result = Process.runSync(Platform.executable, [
+    'format',
+    path,
+  ], stderrEncoding: utf8);
+  if (result.exitCode != 0) {
+    throw Exception('failed to format $path:\n${result.stderr}');
+  }
+}
+
+void main() async {
+  build_constants.main();
+
+  await ffigen.main(['--no-format', '-v', 'severe', '--config', 'ffigen.yaml']);
+  await ffigen.main([
+    '--no-format',
+    '-v',
+    'severe',
+    '--config',
+    'constants-ffigen.yaml',
+  ]);
+
+  _formatFile('lib/src/constant_bindings.g.dart');
+  _formatFile('lib/src/constants.g.dart');
+  _formatFile('lib/src/libc_bindings.g.dart');
+}
