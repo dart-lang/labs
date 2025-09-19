184dd8e6c791228e8ec880209f99894b46c7dbd2
Brian Quinlan <bquinlan@google.com>
2025-09-19 00:19:11 -0700

    More stuff

--- pkgs/unix_api/mobile_test/integration_test/combined_test.dart
+++ pkgs/unix_api/mobile_test/integration_test/combined_test.dart
@@ -1,8 +1,8 @@
 import 'package:integration_test/integration_test.dart';
-import '../../test/dirent_test.dart' as dirent_test;
+import 'package:tests/tests.dart' as tests;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  dirent_test.main();
+  tests.main();
 }
---
+++ pkgs/unix_api/tests/lib/tests.dart
@@ -0,0 +1,10 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+import 'package:tests/src/dirent_test.dart' as direct_test;
+
+void main() {
+  direct_test.main();
+}
