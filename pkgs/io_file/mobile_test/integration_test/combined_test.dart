3398a6a17ab804ed97664c7eb9144b01d6fb930d
Moritz <mosum@google.com>
2025-12-31 10:01:12 +0100

    Add health workflow (#297)

--- pkgs/io_file/mobile_test/integration_test/combined_test.dart
+++ pkgs/io_file/mobile_test/integration_test/combined_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:integration_test/integration_test.dart';
 import '../../test/read_as_bytes_test.dart' as read_as_bytes;
 
